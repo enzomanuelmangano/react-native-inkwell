ab6fd48a0ed89ce38e4c0e06d790737b1df819d3
Enzo Manuel Mangano <enzomanuelmangano@gmail.com>
2021-10-07 19:30:52 +0200

    fix: define measureView as a worklet

--- src/utils/measure.ts
+++ src/utils/measure.ts
@@ -3,6 +3,7 @@ import { measure } from 'react-native-reanimated';
 const measureView = (
   view: Parameters<typeof measure>['0']
 ): ReturnType<typeof measure> | null => {
+  'worklet';
   try {
     return measure(view);
   } catch {
